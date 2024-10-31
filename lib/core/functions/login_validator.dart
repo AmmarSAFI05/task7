550b175a171f52829be4d175223895f86d8e410d
nour <noursheikhdaher@gmail.com>
2024-10-31 23:07:02 +0400

    add validator for phone number

--- lib/core/functions/login_validator.dart
+++ lib/core/functions/login_validator.dart
@@ -13,6 +13,19 @@ class LoginValidator{
     }
     return null;
   }
+//Nour
+  static String? validatePhonenum (String? value) {
+  if (value == null || value.isEmpty) {
+  return 'Please enter your mobile number';
+  }
+  if (!RegExp(r'^[0-9]+$').hasMatch(value)) {
+  return 'Only digits are allowed';
+  }
+  if (value.length != 10) {
+  return 'Mobile number must be 10 digits';
+  }
+  return null;
+  }
 
   // Password Validation in Login
   static String? validatePassword(String? value) {
@@ -21,6 +34,8 @@ class LoginValidator{
     }
     return null;
   }
+
+
 
 
 }
