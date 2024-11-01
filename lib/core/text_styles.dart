f69ab7c7337d57bd96c78f74439166b79fd1b8f6
ammar <ammarsafi2005@gmail.com>
2024-11-01 18:59:18 +0300

    text styles

--- lib/core/text_styles.dart
+++ lib/core/text_styles.dart
@@ -4,6 +4,21 @@ import 'constant/app_fonts.dart';
 import 'constant/app_sizes.dart';
 
 class AppTextStyles {
+  static TextStyle onBoardingTitle = TextStyle(
+      fontSize: AppSize.fontSizeSm() * 1.25,
+      fontWeight: AppFontWeight.bold,
+      color: AppColors.black);
+
+  static TextStyle onBoardingSubTitle = TextStyle(
+      fontSize: AppSize.fontSizeSm(),
+      fontWeight: AppFontWeight.normal,
+      color: AppColors.greyColor);
+
+  static TextStyle buttonText= TextStyle(
+      fontSize: AppSize.fontSizeSm(),
+      fontWeight: AppFontWeight.med,
+      color: AppColors.bgColor);
+
   static TextStyle greyText = const TextStyle(color: AppColors.greyColor);
   static TextStyle homePageUser = TextStyle(
     fontWeight: AppFontWeight.xbold,
